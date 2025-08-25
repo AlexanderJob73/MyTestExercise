330f6df6879659d4747fadff33233592d292773d
AlexanderJob73 <edutkalachev73@gmail.com>
2025-08-25 17:39:33 +0300

    Проект

---
+++ src/main/java/com/example/Zadanie/ZadanieApplication.java
@@ -0,0 +1,15 @@
+package com.example.Zadanie;
+
+import org.asteriskjava.fastagi.AgiServer;
+import org.asteriskjava.fastagi.DefaultAgiServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ZadanieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieApplication.class, args);
+	}
+}
