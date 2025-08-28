543278f2ea8cc61620b8d55d2bb01e69b673456e
AlexanderJob73 <edutkalachev73@gmail.com>
2025-08-28 09:16:46 +0300

    Добавлен протокол UDP

--- src/main/java/com/example/Zadanie/ZadanieApplication.java
+++ src/main/java/com/example/Zadanie/ZadanieApplication.java
@@ -1,10 +1,7 @@
 package com.example.Zadanie;
 
-import org.asteriskjava.fastagi.AgiServer;
-import org.asteriskjava.fastagi.DefaultAgiServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ZadanieApplication {
